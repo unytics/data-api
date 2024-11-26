8db8a465bce4bd33023f80867de3d7b8e09d15b4
Unytics <111615732+unytics@users.noreply.github.com>
2024-11-26 16:41:48 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    """Example Hello World route."""
+    return f"Hello paul!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
