badd5cef03075feaa8eba744bafb2038d724c529
paul.marcombes <paul.marcombes@nickel.eu>
2024-11-26 17:17:10 +0000

    first commit

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
-# data-api
+<p align="center">
+  <img class="hero-image" src="https://github.com/user-attachments/assets/15d2f888-d0e2-493a-adaf-5338fd6dcf53" alt="logo_and_name" style="width: 100%; max-width: 600px">
+</p>
+
+<p align="center">
+    Serve Data with Low-Latency with <em>Data-API</em>
+</p>
+
+
+---
+
+<br>
+
+
+
+## Routes
+
+- `GET /` returns the OpenAPI specification generated on-the-fly from schemas stored in Datastore.
+- `GET /docs` returns the specification rendered with Swagger-UI.
+- `GET /<resource_name>/` returns a list of 100 entities of the kind resource_name.
+- `GET /<resource_name>/<key>` returns the entity of the kind resource_name that has that key.
+- `GET /<resource_name>/foo=bar` returns a list of 100 entities of the kind resource_name where the property/field foo equals bar.
 
 ## Deploy
 
-Run 
+Run
 
 ```
-gcloud run deploy --source .
+gcloud run deploy data-api --source . --set-env-vars "PROJECT=$PROJECT"
 ```
--- main.py
+++ main.py
@@ -1,13 +1,50 @@
+import os
 
-from flask import Flask
+from flask import Flask, request
+import google.cloud.datastore
+from google.cloud.datastore import query as filters
+
+
+import google.auth
+creds, project = google.auth.default()
+
+PROJECT = os.environ.get('PROJECT')
+print(creds.service_account_email)
+
 
 app = Flask(__name__)
+datastore = google.cloud.datastore.Client(project=PROJECT)
+
+
+def string2number(string):
+    if '.' not in string:
+        try:
+            return int(string)
+        except ValueError:
+            pass
+    try:
+        return float(string)
+    except ValueError:
+        pass
+    return string
 
 
 @app.route("/")
 def hello_world():
     """Example Hello World route."""
     return f"Hello paul!"
+
+
+@app.route("/<resource_name>/")
+def list_records(resource_name):
+    query = datastore.query(kind=resource_name)
+    for key, value in request.args.items():
+        if not value:
+            continue
+        value = string2number(value)
+        query.add_filter(filter=filters.PropertyFilter(key, "=", value))
+    results = list(query.fetch())
+    return results
 
 
 if __name__ == "__main__":
